6f25df6e42da143d80c2729098647fc174e4987c
Dan Smith <dev@dotherightthing.co.nz>
2018-05-11 02:50:34 +1200

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPDTRT_Weather
- * @version 	0.0.8
+ * @version 	0.1.0
  * @since       0.7.5
  */
  
