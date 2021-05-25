03c8132d5defb0fbc111afc1bde9b575ef523c1a
Dan Smith <dev@dotherightthing.co.nz>
2021-05-25 21:41:49 +1200

    Sync comment with plugin generator

--- wpdtrt-weather.php
+++ wpdtrt-weather.php
@@ -233,9 +233,12 @@ function wpdtrt_weather_plugin_init() {
 	);
 
 	/**
-	 * Demo shortcode
+	 * Array: demo_shortcode_params
 	 *
-	 * @see https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Settings-page:-Adding-a-demo-shortcode Settings page: Adding a demo shortcode
+	 * Demo shortcode.
+	 *
+	 * See:
+	 * - <Settings page - Adding a demo shortcode: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Settings-page:-Adding-a-demo-shortcode>
 	 */
 	$demo_shortcode_params = array(
 		'name'    => 'wpdtrt_weather_shortcode',
